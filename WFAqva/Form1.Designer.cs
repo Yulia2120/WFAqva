84e3170594cad72f60fbacf47c81a44f32b4f18e
Yulia2120 <ket-dz@rambler.ru>
2022-07-15 15:17:04 +0300

    Добавьте файлы проекта.

---
+++ WFAqva/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WFAqva
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pB = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addFishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.feedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pB
+            // 
+            this.pB.BackColor = System.Drawing.Color.Transparent;
+            this.pB.Image = global::WFAqva.Properties.Resources.giphy__7_;
+            this.pB.Location = new System.Drawing.Point(775, 296);
+            this.pB.Name = "pB";
+            this.pB.Size = new System.Drawing.Size(104, 104);
+            this.pB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pB.TabIndex = 0;
+            this.pB.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 15;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::WFAqva.Properties.Resources.giphy;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 251);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addFishToolStripMenuItem,
+            this.feedToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(181, 92);
+            // 
+            // addFishToolStripMenuItem
+            // 
+            this.addFishToolStripMenuItem.Name = "addFishToolStripMenuItem";
+            this.addFishToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addFishToolStripMenuItem.Text = "Add Fish";
+            this.addFishToolStripMenuItem.Click += new System.EventHandler(this.addFishToolStripMenuItem_Click);
+            // 
+            // feedToolStripMenuItem
+            // 
+            this.feedToolStripMenuItem.Name = "feedToolStripMenuItem";
+            this.feedToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.feedToolStripMenuItem.Text = "Feed";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WFAqva.Properties.Resources._7274829;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(900, 650);
+            this.ContextMenuStrip = this.contextMenuStrip;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pB);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pB;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem addFishToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem feedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+    }
+}
+
---
+++ WFAqva/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WFAqva
+{
+    public partial class Form1 : Form
+    {
+        //int speed = 5;
+        bool leftMove, upMove;
+      //  Point mouse_location = new Point();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+  
+
+        private void Fishs()
+        {
+            if (leftMove == false)
+                pB.Left -= 1;
+            if (leftMove == true)
+                pB.Left += 1;
+            //if (upMove == true)
+            //    pB.Top += 1;
+            //if (upMove == false)
+            //    pB.Top -= 1;
+            if (pB.Left <= ClientRectangle.Left)
+            {
+                leftMove = true;
+
+            }
+            if (pB.Left == 0)
+            {
+                pB.Image.RotateFlip(RotateFlipType.Rotate180FlipY);
+            }
+
+            if (pB.Right >= ClientRectangle.Right)
+            {
+                leftMove = false;
+
+            }
+            if (pB.Right == ClientRectangle.Right)
+            {
+                pB.Image.RotateFlip(RotateFlipType.Rotate180FlipY);
+            }
+
+            //if (pB.Top <= ClientRectangle.Top)
+            //    upMove = true;
+
+            //if (pB.Bottom >= ClientRectangle.Bottom)
+            //    upMove = false;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void addFishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Fishs();
+          
+        }
+
+       
+    }
+}
